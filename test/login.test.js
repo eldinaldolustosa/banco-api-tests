844a07a05c6264b740400b2a6e001f937ba4b0c8
Eldinaldo Lustosa <eldinaldolustosa@gmail.com>
2026-04-18 18:35:58 -0400

    Add FIXTURES to manage requires and helpers

--- test/login.test.js
+++ test/login.test.js
@@ -1,16 +1,15 @@
 const request = require('supertest');
 const {expect} = require('chai');
+const postlogin = require('../fixtures/postLogin.json');
 
 describe('Login', () => {
     describe('POST /login', () => {
         it('Deve retornar 200 com tokem em string quando usar credenciais validas', async () => {
+            const bodyLogin = { ...postlogin }
             const resposta = await request('http://localhost:3000')
                 .post('/login')
                 .set ('Content-Type', 'application/json')
-                .send({
-                    'username': 'julio.lima',
-                    'senha': '123456'
-                })
+                .send(bodyLogin)
             expect(resposta.status).to.equal(200);
             expect(resposta.body.token).to.be.a('string');
         });
--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,39 +1,35 @@
 const request = require('supertest');
 const {expect} = require('chai');
 const {obterToken} = require('../helpers/autenticacao');
+const postTransferencias = require('../fixtures/postTransferencias.json');
 
 describe('Transferencias', () => {
     describe('POST /transferencias', () =>{
-        it('Deve retornar 201 quando a transferencia for realizada com sucesso igual ou acima de $10,00 ', async()=>{
+        let token
+        beforeEach(async() => {
             //capiturar o token do login
-            const token = await obterToken('julio.lima', '123456');
-
+            token = await obterToken('julio.lima', '123456');
+        })
+        
+        it('Deve retornar 201 quando a transferencia for realizada com sucesso igual ou acima de $10,00 ', async()=>{
+            const bodyTransferencias = { ...postTransferencias }
             const resposta = await request('http://localhost:3000')
             .post('/transferencias')
             .set ('Content-Type', 'application/json')
             .set('Authorization', `Bearer ${token}`)
-            .send({
-                'contaOrigem': '1',
-                'contaDestino': '2',
-                'valor': 11.00,
-                'token' : ""
-            })
+            .send(bodyTransferencias)
             expect(resposta.status).to.equals(201)
+            console.log(bodyTransferencias)
         })
         it('Deve retornar 422 quando a conta de origem for abaixo de $10,00', async ()=>{
-            //capiturar o token do login
-            const token = await obterToken('julio.lima', '123456');
+            const bodyTransferencias = { ...postTransferencias }
+            bodyTransferencias.valor = 7.00
 
             const resposta = await request('http://localhost:3000')
             .post('/transferencias')
             .set ('Content-Type', 'application/json')
             .set('Authorization', `Bearer ${token}`)
-            .send({
-                'contaOrigem': '1',
-                'contaDestino': '2',
-                'valor': 9.99,
-                'token' : ""
-            })
+            .send(bodyTransferencias)
             expect(resposta.status).to.equals(422)
         })   
     })
